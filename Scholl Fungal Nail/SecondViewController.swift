4a638d7671a9be8b9ce4289d49250dba096a1229
Timothy Platt <timothy.d.platt@gmail.com>
2019-10-30 13:23:45 +0000

    Initial Commit

---
+++ Scholl Fungal Nail/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Scholl Fungal Nail
+//
+//  Created by Timothy Platt on 30/10/2019.
+//  Copyright © 2019 Timothy Platt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
