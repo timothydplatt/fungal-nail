1f140193af1af9f28fbf8698c6493db5544258bf
Timothy Platt <timothy.d.platt@gmail.com>
2019-11-04 08:19:23 +0000

    Implement machine learning functionality.

---
+++ Scholl Fungal Nail/View Controllers/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Scholl Fungal Nail
+//
+//  Created by Timothy Platt on 30/10/2019.
+//  Copyright © 2019 Timothy Platt. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
